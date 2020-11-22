1edb96895161cdb8a823e93744f6f02814844cd7
Olivier Roques <olivier@oroques.dev>
2020-11-22 14:49:08 +0000

    Update movie page

--- oroques.dev/movies/movies.php
+++ oroques.dev/movies/movies.php
@@ -10,9 +10,9 @@
         <meta name="author" content="Olivier Roques">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <!-- DataTables CSS -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto&family=Roboto+Condensed&display=swap" rel="stylesheet">
         <!-- CSS -->
@@ -27,15 +27,15 @@
             }
         ?>
 
-        <div class="container-fluid my-2">
-            <div class="row align-items-center mt-4">
-                <div class="col-2 offset-1 text-left">
-                    <a class="btn btn-link text-nowrap" href="https://oroques.dev" role="button">Main website</a>
+        <div class="container-xl mt-4">
+            <div class="row align-items-center">
+                <div class="col text-left">
+                    <a class="btn btn-outline-primary text-nowrap" href="https://oroques.dev">Main</a>
                 </div>
-                <div class="col-4 offset-1 text-center" id="title">
+                <div class="col text-center" id="title">
                     <h1>Movies</h1>
                 </div>
-                <div class="col-2 offset-1 text-right">
+                <div class="col text-right">
                     <?php if ($full) {
                         echo "<form action='movies.php' method='post'>\n";
                         echo "<button name='full' value='false' class='btn btn-primary text-nowrap' type='submit'>Reduced list</button>\n";
@@ -46,19 +46,23 @@
                     echo "</form>\n"; ?>
                 </div>
             </div>
-            <hr>
-            <div class="row justify-content-center mt-4">
-                <div class="col-10">
+        </div>
+        <hr>
+        <div class="container-xl">
+            <div class="row">
+                <div class="col">
                     <p>
-                    This page contains the list of films I have watched since July 2012. The data come from a SQL database that is regularly updated. Some columns have been removed.
+                    This page lists all the films I have watched since July 2012.
                     <?php if ($full) {
-                        echo "</br>Entries without <q>seen date</q> are movies I have watched before the creation of this list.";
+                        echo "Entries without <q>seen date</q> are movies I have watched before the creation of this list.";
+                    } else {
+                        echo "This is the reduced version of the list: some columns have been removed.";
                     } ?>
                     </p>
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-10">
+                <div class="col">
                     <table id="movies" class="display">
                         <thead>
                             <tr>
@@ -85,15 +89,15 @@
         <!-- jQuery -->
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <!-- DataTable JS -->
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js" integrity="sha512-rmZcZsyhe0/MAjquhTgiUcb4d9knaFc7b5xAfju483gbEXTkeJRUMIPk6s3ySZMYUHEcjKbjLjyddGWMrNEvZg==" crossorigin="anonymous"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/plug-ins/1.10.21/sorting/datetime-moment.js"></script>
         <script>
             $(document).ready(function() {
                 $.fn.dataTable.moment('MMM YYYY', 'en');
                 $('#movies').DataTable({
                     paging: false,
-                    scrollY: "72vh",
+                    scrollY: "64vh",
                     columns: [
                         {className: "dt-head-center"},
                         {className: "dt-head-center"},
@@ -105,7 +109,7 @@
                             echo '{className: "dt-center dt-nowrap"},';
                         } ?>
                     ],
-                    order: [[5, "desc"], [1, "asc"]],
+                    order: [[5, "desc"], [0, "asc"]],
                     language: {
                         info: "_TOTAL_ out of _MAX_ movies.",
                         search: "Search:",
